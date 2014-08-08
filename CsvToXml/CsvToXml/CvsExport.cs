6d40860360a1cd3c3ea08519900ef80d72b406bb
Ryan Sadwick <rsadwick@gmail.com>
2014-08-08 17:17:23 -0400

    initial commit

---
+++ CsvToXml/CsvToXml/CvsExport.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace CsvToXml {
+    public class CvsExport {
+        private readonly XElement cust;
+
+        public CvsExport() {
+            const string CvsString = @"1,Match these great fashion finds!,338025,http://www.hsn.com/products/rhonda-shear-printed-sleep-set-with-lace-trim/7240561
+2,Watch these tests run wild,546546,http://www.something.net";
+            File.WriteAllText("cust.csv", CvsString);
+            //read into array of strings
+            var source = File.ReadAllLines("cust.csv");
+            cust = new XElement("ignoreMeContainer",
+                from str in source
+                let fields = str.Split(',')
+                select new XElement("_sLevelName",
+                    new XElement("_nLevelTime", fields[0]),
+                    new XElement("_sMessage", fields[1]),
+                    new XElement("_nPointsPerMatch", fields[2])
+                    ));
+        }
+
+        public XElement GetXml() {
+            return cust;
+        }
+    }
+}
---
+++ CsvToXml/CsvToXml/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CsvToXml {
+    internal class Program {
+        private static void Main(string[] args) {
+            var csv2Xml = new CvsExport();
+            Console.WriteLine(csv2Xml.GetXml());
+        }
+    }
+}
